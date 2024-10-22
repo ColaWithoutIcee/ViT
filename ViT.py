262a3a762db90ffed55efb4b865056e0231d0729
Jiabing SUN <jiabingsun777@gmail.com>
2024-10-22 15:06:59 +0800

    first commit

---
+++ ViT.py
@@ -0,0 +1,14 @@
+'''
+# -*- coding: utf-8 -*-
+@File    :   ViT.py
+@Time    :   2024/10/22 15:00:44
+@Author  :   Jiabing SUN 
+@Version :   1.0
+@Contact :   Jiabingsun777@gmail.com
+@Desc    :   None
+'''
+
+# here put the import lib
+import torch
+import torch.nn as nn
+import numpy as np
