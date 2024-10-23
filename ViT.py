2117bda0685d17c6ad97f5a6c388b0b9cd2ce0b0
Jiabing SUN <jiabingsun777@gmail.com>
2024-10-23 15:23:31 +0800

    vit version 1.0

--- ViT.py
+++ ViT.py
@@ -35,6 +35,38 @@ class Mlp(nn.Module):
         return x
 
 
+class TransformerEncoder(nn.Module):
+    
+    def __init__(self, emb_size):
+        super().__init__()
+        self.norm = nn.LayerNorm(emb_size)
+        self.MSA = MultiHeadAttention(emb_size)
+        self.mlp = Mlp(emb_size, emb_size*2)
+    
+    def forward(self, x):
+        x1 = self.norm(x)
+        att_out = self.MSA(x)
+        x2 = x + att_out
+        x3 = self.norm(x2)
+        mlp_out = self.mlp(x3)
+        encoder_out = x2 + mlp_out
+        print(f"encoder_out size {encoder_out.shape}")
+        return encoder_out
+
+class ClassificationHead(nn.Module):
+
+    def __init__(self, emb_size: int=16, n_class: int=10):
+        super().__init__()
+        self.linear = nn.Linear(emb_size, n_class)
+        self.softmax = nn.Softmax(dim=-1)
+    
+    def forward(self, x):
+        x = self.linear(x)
+        x = self.softmax(x)
+        print(f"cls head output: {x.shape}")
+        return x
+
+
 class MultiHeadAttention(nn.Module):
 
     def __init__(self, emb_size: int = 16, n_heads: int = 4, dropout: float = 0.):
@@ -48,27 +80,21 @@ class MultiHeadAttention(nn.Module):
         self.pro = nn.Linear(emb_size, emb_size)
         
     def forward(self, x):
-        print(f"qkv1 shape: {x.shape}")
         qkv = rearrange(self.qkv(x), "b n (h d qkv) -> qkv b h n d", qkv=3, h=self.n_heads)
-        print(f"qkv2 shape {qkv.shape}")
         Queries, Keys, Values = qkv[0], qkv[1], qkv[2]
-        print(f"Queries: {Queries.shape}")
-        print(f"Keys: {Keys.shape}")
-        print(f"Values: {Values.shape}")
         energy = torch.einsum("bhqd, bhkd -> bhqk", Queries, Keys)
-        print(f"Q @ K^T: {energy.shape}")
         energy = energy / self.scaling
         att = F.softmax(energy, dim=-1)
         att = torch.einsum("bhal, bhlv -> bhav", att, Values)
-        print(f"attention: {att.shape}")
         out = rearrange(att, "b h n d -> b n (h d)")
         out =  self.pro(out)
         print(f"output:{out.shape}")
+        return out
 
 
 class Vit(nn.Module):
 
-    def __init__(self, image, patch_size):
+    def __init__(self, image, patch_size, encoder_depth = 7):
         super().__init__()
         self.batch, self.channel, self.image_h, self.image_w  = image.shape
         self.patch_size = patch_size
@@ -79,6 +105,10 @@ class Vit(nn.Module):
         self.cls_token = nn.Parameter(torch.randn(self.batch, 1, self.input_d))
         # Position embedding 
         self.pos_embbing = nn.Parameter(torch.randn(1 + self.channel*(self.image_h // self.patch_size[0])*(self.image_w // self.patch_size[1]), self.input_d))
+        # transformer encoders 
+        self.encoders = nn.Sequential(*[TransformerEncoder(emb_size=16) for _ in range(encoder_depth)])
+        # classification head
+        self.cls_head = ClassificationHead()
     
     def forward(self, images):
         b, c, h, w = images.shape
@@ -86,4 +116,10 @@ class Vit(nn.Module):
         tokens = self.liear_pro(patches)
         tokens = torch.cat((self.cls_token, tokens), dim=1)
         tokens += self.pos_embbing
-        return tokens
+        out = self.encoders(tokens)
+        print(f"encoders out: {out.shape}")
+        # get the classification token only
+        out = out[:,0,:]
+        # classification
+        out = self.cls_head(out)
+        return out
